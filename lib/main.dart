06d8a3c651a12d05ee690780e145638f3793c56a
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2022-05-18 21:44:30 -0500

    change: main home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:am2_library_project/themes/style.dart';
 import 'package:flutter/material.dart';
 import 'package:am2_library_project/screens/main_navigation_menu_screen.dart';
-import 'package:am2_library_project/screens/login_screen.dart';
-import 'package:am2_library_project/screens/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: primaryTheme,
-      home: const LoginScreen(),
+      home: const MainNavigationMenuScreen(),
     );
   }
 }
